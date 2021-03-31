acb0cb6d4c642d2547920ea4fd95b9c583105ab7
Erick Mlndz <erickmlndz@protonmail.com>
2021-03-31 15:40:29 -0700

    major - added credits pop up

--- src/babylon/html/data/CreditLinks.js
+++ src/babylon/html/data/CreditLinks.js
@@ -1,51 +1,91 @@
 export const iconCredits = [
   {
-    company: "",
-    link: "",
-    isLink: true,
-    userName: "",
+    company: "iconify.design",
+    link: "https://iconify.design/",
+    userName: ["Gil Barbara"],
+  },
+  {
+    company: "The Noun Project",
+    link: "https://thenounproject.com/",
+    userName: [
+      "Victoruler",
+      "Calvin Ng",
+      "Marvdrock",
+      "bintang rama",
+      "abdul wahhab",
+      "Creative Stall",
+      "Iconic",
+      "Adrien Coquet",
+      "Valerio Poltrini",
+      "Alice Noir",
+    ],
   },
 ];
 
 export const imageCredits = [
   {
-    company: "",
-    link: "",
-    isLink: true,
-    userName: "",
+    company: "Textures For Planets",
+    link: "https://www.texturesforplanets.com/worlds.shtml",
+    userName: ["Planet textures"],
+  },
+  {
+    company: "Unsplash",
+    link: "https://unsplash.com/",
+    userName: ["@bryangoffphoto"],
+  },
+  {
+    company: "TechMonkeyBusiness",
+    link: "http://www.techmonkeybusiness.com/",
+    userName: ["Hamish"],
   },
 ];
 
 export const audioCredits = [
   {
-    company: "",
-    link: "",
-    isLink: true,
-    userName: "",
+    company: "freesound",
+    link: "https://freesound.org/",
+    userName: ["Robinhood76"],
+  },
+  {
+    company: "Spore",
+    link: "https://www.spore.com/",
+    userName: ["Theme song"],
   },
 ];
 
 export const modelsCredits = [
   {
-    company: "",
-    link: "",
-    isLink: true,
-    userName: "",
-  },  
+    company: "Sketchfab",
+    link: "https://sketchfab.com/",
+    userName: ["Loïc Norgeot"],
+  },
+  {
+    company: "Poly",
+    link: "https://poly.google.com/",
+    userName: ["Rico Balakit"],
+  },
 ];
 
 export const cssCredits = [
   {
-    company: "",
-    link: "",
+    company: "Free Frontend",
+    link: "https://freefrontend.com/",
+    userName: ["collection of CSS"],
+  },
+  {
+    company: "CodePen",
+    link: "https://codepen.io/",
     isLink: true,
-    userName: "",
-  },  
+    userName: ["@fenixmedia", "@pgalor"],
+  },
+  {
+    company: "Code My UI",
+    link: "https://codemyui.com/",
+    userName: ["collection of CSS"],
+  },
 ];
 
-export const specialCredits = [
-  {
-    name: "Janis H",
-    description: "A silly goose ❤",
-  },  
-];
+export const specialCredits = {
+  name: "Janis H",
+  description: "A silly goose ❤",
+};
