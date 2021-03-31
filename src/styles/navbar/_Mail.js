eebb6a2223894b43b404434a33a51b642f6d2d9a
Erick Mlndz <erickmlndz@protonmail.com>
2021-03-31 13:13:57 -0700

    patch reduced mail size

--- src/styles/navbar/_Mail.js
+++ src/styles/navbar/_Mail.js
@@ -2,10 +2,9 @@ import styled from "styled-components";
 
 const _Mail = styled.img`
   transition: filter 500ms ease, transform 4500ms ease-in-out;
-  width: 75px;
   filter: invert(.9) opacity(.5);
   outline: none;
-  width: 3.2rem;
+  width: 3rem;
   &:hover {
     cursor: pointer;
     filter: invert(1);
