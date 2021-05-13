39e22f6dc736b0b5c666d9394c23075c5af139c6
Erick Mlndz <erickmlndz@protonmail.com>
2021-05-13 13:55:16 -0700

    chore: added new data to experience.js

--- src/babylon/html/data/ListOfExperience.js
+++ src/babylon/html/data/ListOfExperience.js
@@ -1,13 +1,13 @@
-import artemisPreview from "../images/artemis_preview.png";
 import { getRandomNumber } from "../utilities/Utilities.js";
 
 export const listOfExperience = [
   {
     company: "Artemis",
+    subtitleCompany: "OSLabs",
     link: "https://project-artemis-v2.vercel.app/",
     from: "2020",
-    to: "2020",
-    preview: artemisPreview,
+    to: "2021",
+    preview: "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/artemis_preview.png",
     descriptions: [
       `
         <p class='inline'>Utilized&nbsp;</p>
@@ -54,11 +54,128 @@ export const listOfExperience = [
         transform: skewX(${getRandomNumber(20)}deg);' title='Typescript'>Typescript</p>
         <p class='inline'>&nbsp;across the stack to create a more predictable code base through explicit type definitions and greatly improve code maintainability by decreasing typecasting errors</p>
       `,
+    ],
+  },
+  {
+    company: "Papyrist",
+    subtitleCompany: "OSLabs",
+    link: "https://opensourcelabs.io/",
+    from: "2020",
+    to: "2021",
+    preview: "https://opensourcelabs.io/images/oslabs-white.png",
+    descriptions: [
       `
-        <p class='inline'>Leveraged&nbsp;</p>
+        <p class='inline'>Incorporated&nbsp;</p>
         <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
-        transform: skewX(${getRandomNumber(20)}deg);' title='Git'>Git</p>
-        <p class='inline'>&nbsp;to monitor changes to the code base and prevent breaking changes as well as deploy git bisect to debug and backtrack to a stable version of Artemis</p>
+        transform: skewX(${getRandomNumber(20)}deg);' title='Styled-Components'>Styled-Components</p>
+        <p class='inline'>&nbsp;to leverage Automatic Critical CSS benefits by having the client render only what is necessary and further separate CSS styling and Javascript logic within React Framework</p>
+      `,
+      `
+        <p class='inline'>Deployed&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='React Router'>React Router</p>
+        <p class='inline'>&nbsp;to enhance UX by reducing unnecessary full-page DOM re-renders and create a smoother transition between pages</p>
+      `,
+      `
+        <p class='inline'>Constructed a&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='Node.js'>Node.js</p>
+        <p class='inline'>&nbsp;and&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='Express.js'>Express.js</p>
+        <p class='inline'>&nbsp;backend to facilitate a faster response time with Node’s non-blocking asynchronous request handling</p>
+      `,
+    ],
+  },
+  {
+    company: "ChatterBox",
+    subtitleCompany: "OSLabs",
+    link: "https://opensourcelabs.io/",
+    from: "2020",
+    to: "2021",
+    preview: "https://opensourcelabs.io/images/oslabs-white.png",
+    descriptions: [
+      `
+        <p class='inline'>Adopted&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='SASS'>SASS</p>
+        <p class='inline'>&nbsp;to improve extensibility and readability of stylesheets and ensure that the design of each component remained modular and independent</p>
+      `,
+      `
+        <p class='inline'>Overhauled the&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='PostgreSQL'>PostgreSQL</p>
+        <p class='inline'>&nbsp;database to be normalized to the 3rd form and remove unnecessary joins when querying</p>
+      `,
+      `
+        <p class='inline'>Encrypted user data with&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='BCrypt'>BCrypt</p>
+        <p class='inline'>&nbsp;to keep sensitive user information secure and prevent brute force or rainbow table attacks via randomized salt rounds</p>
+      `,
+    ],
+  },
+  {
+    company: "PawFinder",
+    subtitleCompany: "Healthy Spot",
+    link: "https://pawfinder.com/",
+    from: "2018",
+    to: "2019",
+    preview: "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/pawFinderImage-min.png",
+    descriptions: [
+      `
+        <p class='inline'>Overhauled application&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='React Hooks'>React Hooks</p>
+        <p class='inline'>&nbsp;to share logic between multiple components using custom hooks and to reduce bloat and increase readability for future developers</p>
+      `,
+      `
+        <p class='inline'>Connected a&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='MongoDB'>MongoDB</p>
+        <p class='inline'>&nbsp;to achieve high-speed query responses and lessen the need for complex queries by storing related data in single documents while using&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='Mongoose'>Mongoose</p>
+        <p class='inline'>&nbsp;to communicate with the database to ensure database schemas remained consistent when manipulating documents</p>
+      `,
+      `
+        <p class='inline'>Configured&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='Webpack'>Webpack</p>
+        <p class='inline'>&nbsp;to utilize hot module reloading for faster debugging and to enable the help of loaders, such as Babel,  to transpile ES6 into ES5 Syntax</p>
+      `,
+    ],
+  },
+  {
+    company: "PawAcquisition",
+    subtitleCompany: "Healthy Spot",
+    link: "https://healthyspot.com/",
+    from: "2018",
+    to: "2019",
+    preview: "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/healthy_spot.png",
+    descriptions: [
+      `
+        <p class='inline'> Accompanied brand ambassadors to collect data on people we meet on the streets in an&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='Excel Spreadsheet'>Excel Spreadsheet</p>
+      `,
+      `
+        <p class='inline'>Mapped out statistically relevant data within&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='Python'>Python</p>
+        <p class='inline'>&nbsp;for brand ambassadors to better target customer acquisition</p>
+      `,
+      `
+        <p class='inline'>Designed a layout with&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='Javascript'>Javascript</p>
+        <p class='inline'>,&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='HTML'>HTML</p>
+        <p class='inline'>, and&nbsp;</p>
+        <p class='inline_flex glitch_paragraph' style='animation: glitch ${getRandomNumber(3) + .75}s alternate infinite;
+        transform: skewX(${getRandomNumber(20)}deg);' title='CSS'>CSS</p>
+        <p class='inline'>&nbsp;to better visualize statistical data</p>
       `,
     ],
   },
