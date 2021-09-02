6ded17e8c027801fb21b3553fa9d0dcb7d029e53
Erick Mlndz <erickmlndz@protonmail.com>
2021-09-02 13:59:19 -0700

    chore: commented out star logic

--- src/components/landing_page/LandingPage.jsx
+++ src/components/landing_page/LandingPage.jsx
@@ -4,11 +4,11 @@ import _SpaceBackground from "../../styles/landing_page/_SpaceBackground.js";
 import ShinningStar from "./ShinningStar.jsx";
 
 const LandingPage = () => {
-  const AMOUNT_OF_STARS = 10;
-  const stars = [];
-  for (let i = 0; i < AMOUNT_OF_STARS; i += 1) {
-    stars.push(<ShinningStar key={i} />);
-  }
+  // const AMOUNT_OF_STARS = 10;
+  // const stars = [];
+  // for (let i = 0; i < AMOUNT_OF_STARS; i += 1) {
+  //   stars.push(<ShinningStar key={i} />);
+  // }
 
   return (
     <section>
