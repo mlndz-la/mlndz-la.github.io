8a704ae599c836d8b2c76ad818149f518337ef13
Erick Mlndz <erickmlndz@protonmail.com>
2021-09-02 17:13:47 -0700

    chore: added new experience

--- src/babylon/html/data/ListOfExperience.js
+++ src/babylon/html/data/ListOfExperience.js
@@ -1,158 +1,134 @@
 export const listOfExperience = [
   {
-    company: "Artemis",
-    subtitleCompany: "OSLabs",
+    product: "Digital Buyflow",
+    company: "Centerfield",
+    link: "https://www.centerfield.com/",
+    from: "Jun 2020",
+    to: "Aug 2021",
+    preview: "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/Centerfield-logo.png",
+    descriptions: [
+      `
+        <p>Utilized <b>React</b> to create reusable components and leverage the reconciliation algorithm in order to re-render parts of the DOM that have changed
+        </p>
+      `,
+      `
+        <p>Capitalized on <b>React PropTypes</b> to configure and guarantee controlled component rendering as well as provide explicit error handling
+        </p>
+      `,
+      `
+        <p>Applied <b>Redux</b> to create a centralized data store that reduces prop drilling, enhances scalability by logically organizing state, and with Immutable.js, ensures data immutability for an improved debugging experience
+        </p>
+      `,
+      `
+        <p>Adopted <b>SASS</b> to improve extensibility and readability of stylesheets and ensure that the design of each component remained modular and independent</p>
+      `,
+      `
+        <p>Used <b>Redux Saga</b> Generators to handle async calls for authentication & dynamically generated data improving determinism of async testing</p>
+      `,
+      `
+        <p>Wrote <b>Jest</b>-based end-to-end, integration and unit tests in Cypress.io to drive development and  verify application behavior by mocking user interactions</p>
+      `,
+      `
+        <p>Deployed <b>React-Router</b> to enhance UX by reducing unnecessary full-page DOM re-renders and create a smoother transition between pages</p>
+      `,
+    ],
+  },
+  {
+    product: "Artemis",
+    company: "OSLabs",
     link: "https://project-artemis-v2.vercel.app/",
-    from: "2020",
-    to: "2021",
+    from: "Apr 2020",
+    to: "May 2021",
     preview: "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/artemis_preview.png",
     descriptions: [
       `
-        <p class='inline'>Utilized</p>
-        <p class='inline'><b>React</b></p>
-        <p class='inline'>to create reusable components and leverage the reconciliation algorithm in order to re-render parts of the DOM that have changed</p>
+        <p>Implemented <b>React Context API</b> to consume and provide a single source of truth, simplify the data flow between components and maintain a cleaner codebase by removing needless prop drilling from stateless components</p>
       `,
       `
-        <p class='inline'>Implemented</p>
-        <p class='inline'><b>React Context API</b></p>
-        <p class='inline'>to consume and provide a single source of truth, simplify the data flow between components and maintain a cleaner codebase by removing needless prop drilling from stateless components</p>
+        <p>Outlined a modern <b>Neomorphic UI</b> with CSS Grid and leveraged CSS Flexbox to create a responsive, mobile-first UI regardless of screen size</p>
       `,
       `
-        <p class='inline'>Outlined a modern Neomorphic UI with</p>
-        <p class='inline'><b>CSS Grid</b></p>
-        <p class='inline'>and leveraged</p>
-        <p class='inline'><b>CSS Flexbox</b></p>
-        <p class='inline'>to create a responsive, mobile-first UI regardless of screen size</p>
+        <p>Selected <b>Aleph</b> to build a dynamic React Application and pre-render the application server-side at build time before displaying to the client for quicker initial load times as well as update Artemis, through Hot Module Reloading, during development without having to restart the server on every change
+        </p>
       `,
       `
-        <p class='inline'>Selected</p>
-        <p class='inline'><b>Aleph</b></p>
-        <p class='inline'>to build a dynamic React Application and pre-render the application server-side at build time before displaying to the client for quicker initial load times as well as update Artemis, through Hot Module Reloading, during development without having to restart the server on every change</p>
+        <p>Integrated <b>Chart.js</b> for its minimalistic design and small module size</p>
       `,
       `
-        <p class='inline'>Integrated</p>
-        <p class='inline'><b>Chart.js</b></p>
-        <p class='inline'>for its minimalistic design and small module size plus its added bonus of minimal lag on the clients end</p>
+        <p>Architected a <b>Deno</b> server to produce a lightweight application with Deno’s module system and safeguarded Artemis by utilizing its security measures to prevent unwanted script injections or file access from modules at runtime</p>
       `,
       `
-        <p class='inline'>Architected a</p>
-        <p class='inline'><b>Deno</b></p>
-        <p class='inline'>server to produce a lightweight application with Deno’s module system and safeguarded Artemis by utilizing its security measures to prevent unwanted script injections or file access from modules at runtime</p>
+        <p>Administered <b>Typescript</b> across the stack to create a more predictable code base through explicit type definitions and greatly improve code maintainability by decreasing type casting errors</p>
       `,
       `
-        <p class='inline'>Applied</p>
-        <p class='inline'><b>Typescript</b></p>
-        <p class='inline'>across the stack to create a more predictable code base through explicit type definitions and greatly improve code maintainability by decreasing typecasting errors</p>
-      `,
-      `
-        <p class='inline'>Devised complex traversal algorithms to pinpoint performance and potential bottlenecks of</p>
-        <p class='inline'><b>GraphQL</b></p>
-        <p class='inline'>queries by analyzing depth of nested queries, number of unique database requests, and effective runtime of roundtrip</p>
+        <p>Devised complex traversal algorithms to pinpoint performance and potential bottlenecks of <b>GraphQL</b> queries by analyzing depth of nested queries, number of unique database requests, and effective runtime of roundtrip</p>
       `,
     ],
   },
   {
-    company: "Papyrist",
-    subtitleCompany: "OSLabs",
+    product: "Papyrist",
+    company: "OSLabs",
     link: "https://opensourcelabs.io/",
-    from: "2020",
-    to: "2021",
+    from: "Apr 2020",
+    to: "May 2021",
     preview: "https://opensourcelabs.io/images/oslabs-white.png",
     descriptions: [
       `
-        <p class='inline'>Incorporated</p>
-        <p class='inline'><b>Styled-Components</b></p>
-        <p class='inline'>to leverage Automatic Critical CSS benefits by having the client render only what is necessary and further separate CSS styling and Javascript logic within React Framework</p>
+        <p>Constructed a <b>Node.js</b> and <b>Express.js</b> backend to facilitate a faster response time with Node’s non-blocking asynchronous request handling</p>
       `,
       `
-        <p class='inline'>Deployed</p>
-        <p class='inline'><b>React Router</b></p>
-        <p class='inline'>to enhance UX by reducing unnecessary full-page DOM re-renders and create a smoother transition between pages</p>
-      `,
-      `
-        <p class='inline'>Constructed a</p>
-        <p class='inline'><b>Node.js</b></p>
-        <p class='inline'>and</p>
-        <p class='inline'><b>Express.js</b></p>
-        <p class='inline'>backend to facilitate a faster response time with Node’s non-blocking asynchronous request handling</p>
+        <p>Encrypted user data with <b>BCrypt</b> to keep sensitive user information secure and prevent brute force or rainbow table attacks via randomized salt rounds</p>
       `,
     ],
   },
   {
-    company: "ChatterBox",
-    subtitleCompany: "OSLabs",
+    product: "ChatterBox",
+    company: "OSLabs",
     link: "https://opensourcelabs.io/",
-    from: "2020",
-    to: "2021",
+    from: "Apr 2020",
+    to: "May 2021",
     preview: "https://opensourcelabs.io/images/oslabs-white.png",
     descriptions: [
       `
-        <p class='inline'>Adopted</p>
-        <p class='inline'><b>SASS</b></p>
-        <p class='inline'>to improve extensibility and readability of stylesheets and ensure that the design of each component remained modular and independent</p>
-      `,
-      `
-        <p class='inline'>Overhauled the</p>
-        <p class='inline'><b>PostgreSQL</b></p>
-        <p class='inline'>database to be normalized to the 3rd form and remove unnecessary joins when querying</p>
-      `,
-      `
-        <p class='inline'>Encrypted user data with</p>
-        <p class='inline'><b>BCrypt</b></p>
-        <p class='inline'>to keep sensitive user information secure and prevent brute force or rainbow table attacks via randomized salt rounds</p>
+        <p>Overhauled the <b>PostgreSQL</b> database to be normalized to the 3rd form and remove unnecessary joins when querying</p>
       `,
     ],
   },
   {
-    company: "PawFinder",
-    subtitleCompany: "Healthy Spot",
+    product: "Paw Finder",
+    company: "Healthy Spot",
     link: "https://pawfinder.com/",
-    from: "2018",
-    to: "2019",
+    from: "Feb 2018",
+    to: "Dec 2019",
     preview: "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/pawFinder.png",
     descriptions: [
       `
-        <p class='inline'>Overhauled application</p>
-        <p class='inline'><b>React Hooks</b></p>
-        <p class='inline'>to share logic between multiple components using custom hooks and to reduce bloat and increase readability for future developers</p>
+        <p>Overhauled application with <b>React hooks</b> to share logic between multiple components using custom hooks and increase readability for future developers</p>
       `,
       `
-        <p class='inline'>Connected a</p>
-        <p class='inline'><b>MongoDB</b></p>
-        <p class='inline'>to achieve high-speed query responses and lessen the need for complex queries by storing related data in single documents while using</p>
-        <p class='inline'><b>Mongoose</b></p>
-        <p class='inline'>to communicate with the database to ensure database schemas remained consistent when manipulating documents</p>
+        <p>Connected a <b>MongoDB</b> to achieve high-speed query responses and lessen the need for complex queries by storing related data in single documents while using <b>Mongoose</b> to communicate with the database to ensure database schemas remained consistent when manipulating documents</p>
       `,
       `
-        <p class='inline'>Configured</p>
-        <p class='inline'><b>Webpack</b></p>
-        <p class='inline'>to utilize hot module reloading for faster debugging and to enable the help of loaders, such as Babel,  to transpile ES6 into ES5 Syntax</p>
+        <p>Incorporated <b>Styled-Components</b> to leverage Automatic Critical CSS benefits  by having the client render only what is necessary and further separate CSS styling and Javascript logic within React Framework</p>
       `,
     ],
   },
   {
-    company: "PawAcquisition",
-    subtitleCompany: "Healthy Spot",
+    product: "Paw Acquisition",
+    company: "Healthy Spot",
     link: "https://healthyspot.com/",
-    from: "2018",
-    to: "2019",
+    from: "Feb 2018",
+    to: "Dec 2019",
     preview: "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/healthy_spot.png",
     descriptions: [
       `
-        <p class='inline'>Accompanied brand ambassadors to collect data on people we meet on the streets in an</p>
-        <p class='inline'><b>Excel Spreadsheet</b></p>
+        <p>Accompanied brand ambassadors to collect data on people we meet on the streets in an <b>Excel Spreadsheet</b></p>
       `,
       `
-        <p class='inline'>Mapped out statistically relevant data within</p>
-        <p class='inline'><b>Python</b></p>
-        <p class='inline'>for brand ambassadors to better target customer acquisition</p>
+        <p>Mapped out statistically relevant data within <b>Python</b> for brand ambassadors to better target customer acquisition</p>
       `,
       `
-        <p class='inline'>Designed a layout with</p>
-        <p class='inline'><b>Javascript</b>,</p>
-        <p class='inline'><b>HTML</b>, and</p>
-        <p class='inline'><b>CSS</b></p>
-        <p class='inline'>to better visualize data</p>
+        <p>Designed a layout with <b>Javascript</b>, <b>HTML</b>, and <b>CSS</b> to better visualize data</p>
       `,
     ],
   },
