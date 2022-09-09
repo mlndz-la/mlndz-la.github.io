eb54dc7a0eb93e1da0ba305e110fef3136a2b7b2
Erick Mlndz <erickmlndz@protonmail.com>
2022-09-09 16:06:17 -0700

    reset website pt2 - clear code

--- src/styles/_SpaceBackGroundTwo.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import space from "../images/space2.jpg";
-
-const _SpaceBackgroundTwo = styled.div`
-  background-image: url(${space});
-  background-size: cover;
-`;
-
-export default _SpaceBackgroundTwo;
