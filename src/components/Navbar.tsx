5241380cd38470b2d0907371d1ee66e2be0fdec5
Erick Mlndz <erickmlndz@protonmail.com>
2023-01-09 12:24:06 -0800

    add: breakpoints for desktop + styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,13 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <a className="navbar_logo">Erick Melendez</a>
-      <div className="navbar_links_container">
-        <a className="navbar_link">Work</a>
-        <a className="navbar_link">Resume</a>
-        <a className="navbar_link">Contact</a>
+      <div className="navbar_container">
+        <a className="navbar_logo">Erick Melendez</a>
+        <div className="navbar_links_container">
+          <a className="navbar_link">Work</a>
+          <a className="navbar_link">Resume</a>
+          <a className="navbar_link">Contact</a>
+        </div>
       </div>
     </nav>
   );
