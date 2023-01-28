12ec5bd837a1c415a45c31c57dc8c4ee2f8cb818
Erick Mlndz <erickmlndz@protonmail.com>
2023-01-27 16:52:21 -0800

    complete: www feature

--- src/PawFinder.tsx
+++ src/PawFinder.tsx
@@ -1,4 +1,5 @@
 import { CaseStudyTitle } from "./reusable/Titles/CaseStudyTitle";
+import { WhoWhatWhen } from "./reusable/WhoWhatWhen/WhoWhatWhen";
 import { DisplayCategories } from "./reusable/Categories/DisplayCategories";
 import { TextColorEnum } from "./reusable/Titles/types";
 import { ThemesEnum } from "./reusable/Categories/types";
@@ -6,6 +7,15 @@ import "./PawFinder.css";
 
 // * TEST DATA
 const list = ["Market Research", "Competitive Analysis", "Personas", "Flow Diagram", "Paper Wireframes", "Low-fidelity UI", "High-fidelity UI", "Prototype", "Usability Study", "Accessibility Evaluation",]
+
+const whoWhatWhenData = [
+  {title: "Where", subtitle: "United States, California"},
+  {title: "What", subtitle: "IOS based mobile app"},
+  {title: "Why", subtitle: "Pet Adoption"},
+  {title: "Role", subtitle: "Designer, Researcher"},
+  {title: "Category", subtitle: "Pet, House & Home"},
+  {title: "When", subtitle: "2022"},
+]
 
 export const PawFinder = () => (
   <>
@@ -25,7 +35,7 @@ export const PawFinder = () => (
           </div>
           <div className="paw_finder_line"></div>
           <div className="www_and_case_study_container">
-            <div className="www_TEST">WWW</div>
+            <WhoWhatWhen theme={TextColorEnum.dark} data={whoWhatWhenData}/>
             <div>TheRestOfTheCaseStudy</div>
         </div>
       </div>
