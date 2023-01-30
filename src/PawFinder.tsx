a8e3f4d89671256637fe002a896ac0e136bafce8
Erick Mlndz <erickmlndz@protonmail.com>
2023-01-30 13:20:41 -0800

    task: removed dummy data & updated with real data

--- src/PawFinder.tsx
+++ src/PawFinder.tsx
@@ -3,42 +3,34 @@ import { WhoWhatWhen } from "./reusable/WhoWhatWhen/WhoWhatWhen";
 import { DisplayCategories } from "./reusable/Categories/DisplayCategories";
 import { TextColorEnum } from "./reusable/Titles/types";
 import { ThemesEnum } from "./reusable/Categories/types";
+import { pawFinderData } from "./data/PawFinder";
 import "./PawFinder.css";
 
-// * TEST DATA
-const list = ["Market Research", "Competitive Analysis", "Personas", "Flow Diagram", "Paper Wireframes", "Low-fidelity UI", "High-fidelity UI", "Prototype", "Usability Study", "Accessibility Evaluation",]
+export const PawFinder = () => {
+  const data = pawFinderData;
+  const {whatWhereWhy, categories, caseStudyTitle, caseStudySubtitle} = data.caseStudyStart;
 
-const whoWhatWhenData = [
-  {title: "Where", subtitle: "United States, California"},
-  {title: "What", subtitle: "IOS based mobile app"},
-  {title: "Why", subtitle: "Pet Adoption"},
-  {title: "Role", subtitle: "Designer, Researcher"},
-  {title: "Category", subtitle: "Pet, House & Home"},
-  {title: "When", subtitle: "2022"},
-]
-
-export const PawFinder = () => (
-  <>
+  return (<>
     <div className="banner_TEST">Banner</div>
     <div className="title_categories_case_study_and_www_container">
         <CaseStudyTitle
-            title='Paw Finder'
-            subtitle="Pet adoption, now easier than ever"
+            title={caseStudyTitle}
+            subtitle={caseStudySubtitle}
             textColor={TextColorEnum.dark}
         />
         <div className="categories_www_and_case_study_container">
           <div className="paw_finder_categories_container">
             <DisplayCategories
-              categoryList={list}
+              categoryList={categories}
               theme={ThemesEnum.pf}
             />
           </div>
           <div className="paw_finder_line"></div>
           <div className="www_and_case_study_container">
-            <WhoWhatWhen theme={TextColorEnum.dark} data={whoWhatWhenData}/>
+            <WhoWhatWhen theme={TextColorEnum.dark} data={whatWhereWhy}/>
             <div>TheRestOfTheCaseStudy</div>
         </div>
       </div>
     </div>
-  </>
-);
+  </>)
+};
