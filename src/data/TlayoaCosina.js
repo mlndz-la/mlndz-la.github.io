6a37e09d29452799555721e5875cec79985c4479
Erick Mlndz <erickmlndz@protonmail.com>
2023-01-30 13:20:06 -0800

    add: updated data to js format

---
+++ src/data/TlayoaCosina.js
@@ -0,0 +1,217 @@
+export const tlayoaCosinaData = {
+  "mockUpImageTopMobile": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Mock-up3-min.png",
+  "mockUpImageTopDesktop": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Mock-up_landing-min.png",
+  "caseStudyStart": {
+    "caseStudyTitle": "Tlayoa's Cosina",
+    "caseStudySubtitle": "Authentic Mexican food at your finger tips",
+    "caseStudyCategories": [
+      "Market Research",
+      "Competitive Analysis",
+      "User Survey",
+      "Personas",
+      "Flow Diagram",
+      "Low-fidelity UI",
+      "High-fidelity UI",
+      "Prototype",
+      "Usability Study",
+      "Accessibility Evaluation"
+    ],
+      whatWhereWhy: [
+        {title: "Where", subtitle: "United States, California"},
+        {title: "What", subtitle: "Mobile App (Android)"},
+        {title: "Why", subtitle: "Family Restaurant"},
+        {title: "Role", subtitle: "Designer, Researcher"},
+        {title: "Category", subtitle: "Food & Drink, Restaurant"},
+        {title: "When", subtitle: "2022"},
+    ]
+  },
+  "whyThisProduct": {
+    "title": "Why I chose this product",
+    "desc": "This is an homage to my parents life long dream to open up a restaurant in the US. The app allows people to easily attain authentic Mexicans food on there phones.",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/croc_arrow.png"
+  },
+  "marketResearch": {
+    "title": "Market Research",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/chart1.png",
+    "theClaim": {
+      "title": "The Claim",
+      "desc": "For the past 10 years the US has had a steady increase year by year in the Mexican cuisines space by 100B and it is expected to explode in 2023. The current data shows that it could jump as much as 560B in 2023."
+    },
+    "theProblem": {
+      "title": "The Problem",
+      "desc": "As a result, many restaurants across California have seen an increase in Mexican-style tacos. Not many of them can cook a wide variety of Mexican cuisines. Most Mexican style restaurants serve tacos or tamales as their main dish."
+    }
+  },
+  "competitiveAnalysis": {
+    "title": "Competitive Analysis",
+    "desc": "I analyzed 3 restaurants local to Los Angeles, California - I looked at the user flow and searched for any negative reviews in the app store or any online reviews.",
+    "chickenItza": {
+      "title": "Chicken Itza",
+      "logo": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/21496logo.png"
+    },
+    "LAIndianaTamales": {
+      "title": "LA Indiana Tamales",
+      "logo": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/logo_la_indiana.png"
+    },
+    "tamalesLosAngeles": {
+      "title": "Tamales Los Angeles",
+      "logo": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/logo_-_CAC620e63c007ae1.png"
+    },
+    "theGood": {
+      "title": "The Good",
+      "desc": "All apps allow users to browse the store with out having to create an account. Chicken Itza has a particularly robust search engine for their menu."
+    },
+    "theBad": {
+      "title": "The Bad",
+      "desc": "They're focus on a wide variety of Mexican cuisines is lacking (Indiana Tamales, Tamales Los Angeles). The user experience is lacking for LA Indiana Tamales, particularly the ability to navigate and search through the menu."
+    },
+    "comments": {
+      "julia": {
+        "name": "Julia",
+        "profilePic": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/julia.png",
+        "comment": "The app is really difficult to navigate. I can't search for keywords and I'm unsure of where to find them."
+      },
+      "markus": {
+        "name": "Markus",
+        "profilePic": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/markus.png",
+        "comment": "Tamales are great but I'd like to try other Mexicans foods - too many tamales IMO."
+      },
+      "sophie": {
+        "name": "Sophie",
+        "profilePic": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/sophie.png",
+        "comment": "I always love to see what others have to say about the restaurant."
+      },
+      "jacob": {
+        "name": "Jacob",
+        "profilePic": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/jacob.png",
+        "comment": "People have to realize, when you make an app it can't be boring."
+      }
+    }
+  },
+  "userSurvey": {
+    "title": "User Survey",
+    "desc": "I conducted a quick survey among people who are in Los Angeles, CA and are seeking an authentic Mexican restaurant on Reddit.",
+    "redditInfoGraphic": {
+      "title": "What's the most important factor you take when it comes to looking for a restaurant?",
+      "subtitle": "26 Participants",
+      "redditLogo": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/reddit-icon.png",
+      "surveyCategories": {
+        "categoryOne": ["Price", "31%"],
+        "categoryTwo": ["Flavor", "25%"],
+        "categoryThree": ["Variety", "34%"],
+        "categoryFour": ["Other (please comment)", "10%"]
+      },
+      "notableComments": {
+        "olivia": {
+          "name": "Olivia",
+          "profilePic": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/olivia.png",
+          "comment": "A variety of dishes is great but you still have to be able to do the basics tacos, tamales, etc."
+        },
+        "dave": {
+          "name": "Dave",
+          "profilePic": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/dave.png",
+          "comment": "I'd like to know if the food I pick out will be spicy or not, Too many Mexican dishes are spicy for me."
+        },
+        "jackson": {
+          "name": "Jackson",
+          "profilePic": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/jackson.png",
+          "comment": "The most important thing for myself is the quality of the service."
+        },
+        "jasper": {
+          "name": "Jasper",
+          "profilePic": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/jasper.png",
+          "comment": "I like it when the app lets me take control of my search results."
+        }
+      }
+    }
+  },
+  "researchSoFar": {
+    "title": "The initial research shows",
+    "desc": "There is a growing need for authentically cooked Mexican cuisines with an affordably priced menu items and a wide variety of options.",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/sit_and_talk.png"
+  },
+  "personas": {
+    "title": "Personas",
+    "desc": "I created personas based on two types of users of the app, the factory worker and the student.",
+    "topImage": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Persona-min.png",
+    "bottomImage": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Persona-1.png"
+  },
+  "timeToStartDesign": {
+    "title": "Time to start designing!",
+    "desc": "Once I collected and synthesized through all my research data, it was time to sketch out the first flows and the initial low-fidelity wireframes.",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/croc_arrow_flip.png"
+  },
+  "flowDiagram": {
+    "title": "Flow Diagram",
+    "desc": "To outline all the necessary functionality I created a simple flow diagram of the main tasks the users can do. This particular flow shows a successful user session but others were created such as fail state flows.",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Frame3-min.png",
+    "imageTitle": "Main Client Flow"
+  },
+  "lowFidelityUIDesign": {
+    "title": "Low-fidelity UI Design",
+    "desc": "Once the flow diagram was established I started creating the low fidelity wire frames of the main flow. This panel shows how a user might add to cart an item.",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/low-fid-min.png"
+  },
+  "highFidelityUIDesign": {
+    "title": "High-fidelity UI Design",
+    "desc": "As soon as I finished the first iteration of the app's flow, I dove right into making the app's primary screens. Initially, I set the style guidelines, including the fonts and colors.",
+    "alphabet": "AaBbCcDdEeFfGgHh",
+    "color": {
+      "title": "Color Palette",
+      "desc": "Accent, Primary, Secondary, Tertiary, Background"
+    },
+    "font": {
+      "title": "Font",
+      "desc": "Proxima Nova Regular, Medium, Bold, Extra Bold"
+    },
+    "look": {
+      "title": "Modern minimal look & feel",
+      "desc": "This style was achieved by using sharp edges along with a soft color scheme and an elegant font logo."
+    }
+  },
+  "highFidelityDesign": {
+    "desc": "Including two different navigation patterns (a tab bar and taskbar) that could be used for A/B testing in further design stages.",
+    "screenshots": [
+      "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Menu-1-min.png",
+      "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Homepage-1-min.png",
+      "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/About-1.png",
+      "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Search-1-min.png",
+      "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Shopping-List-1.png",
+      "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/Product-Page-1-min.png"
+    ],
+    "imageDesc": "I also tested a white background to give the app a more elegant look and feel on the main screens. As well as giving the product preview a square viewing window."
+  },
+  "highFidelityPrototype": {
+    "title": "High- fidelity Prototype",
+    "desc": "I connected my high fidelity designs into a clickable prototype. That will allow me to test the app on a first group of users.",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/buy-flow.png",
+    "imageDesc": "The prototype can  be live-previewed at: [ link ]",
+    "link": ""
+  },
+  "usabilityStudy": {
+    "title": "Usability Study",
+    "desc1": "I conducted a unmoderated usability study with 5 users. Each were given a prototype and a set of prompts to follow. The prompts were designed to help me better understand if my initial user flow was easy to follow and navigate. Can a user add items, delete items or learn about a product?",
+    "desc2": "This was tested with the help of Google slides and recording software of their choice. Their was an initial Zoom call to help the user understand what the usability study entails and getting proper consent for any data collected."
+  },
+  "studyResults": {
+    "title": "Study Results",
+    "desc": "80% of users (4 of 5) were frustrated by the fact that users couldn't go back step from the product page. All users were able to add and delete items, but no one expected there to be more images by clicking on the product image itself. This particular study had a success rate of 20% "
+  },
+  "futureConsiderations": {
+    "title": "Future Considerations",
+    "desc": "Adding an accessible “back button” to each of the pages would be top priority seeing as how that was a major pain point. Adding a carousel with dots at the bottom of an image, would help users identify that there are more images to view.",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/update-concept.png"
+  },
+  "accessibilityCheck": {
+    "title": "Accessibility Check",
+    "desc1": "The app has been evaluated for contrast to match the AA standards of WCAG. In some cases I found the contrast could have been improved. ",
+    "desc2": "In one example, the star (rating system) blends into the background color too much, making it difficult for user to see. I decided too create a black outline of the star so it can pop out more easily. The same was done for the spicy meter for consistency.",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/WCAG-2.png"
+  },
+  "productOverview": {
+    "title": "Product Summary",
+    "desc": "During the product life-cycle, I evaluated the market, created a Reddit user survey, performed an unmoderated usability study, made a set of low-fidelity wireframes, connected these frames into a prototype and built out a high-fidelity wire frame with a beautiful UI design. As a last step I conducted an extensive QA audit focusing on consistency and color contrast with regards to accessibility.",
+    "image": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/croc_arrow.png"
+  },
+  "mockUpImageBottom": "https://raw.githubusercontent.com/mlndz-la/pwAssets/main/TC/no-aurora-Mock-up-min.png"
+}
