07fecfa2fccfd32f47aa049872a2c6f4477c1eb0
Janis-H <janis_h.aguilar@yahoo.com>
2023-02-06 23:38:33 -0800

    added cursor pointer on CaseStudyPreview_img:hover

--- src/components/CaseStudyPreview/CaseStudyPreview.tsx
+++ src/components/CaseStudyPreview/CaseStudyPreview.tsx
@@ -8,11 +8,11 @@ export const CaseStudyPreview = ({ imagePreview, title, subtitle, caseStudyUrl }
   const goToCaseStudy = () => navigate(caseStudyUrl);
 
   return (
-    <div className="case_study_preview_container" >
-      <img className="case_study_preview_container__img" src={imagePreview} onClick={goToCaseStudy} />
-      <h3 className="case_study_preview_container__title" >{title}</h3>
-      <p className="case_study_preview_container__subtitle" >{subtitle}</p>
-      <h4 className="case_study_preview_container__link" onClick={goToCaseStudy}>Read Case Study</h4>
+    <div className="case_study_preview__container" >
+      <img className="case_study_preview__img" src={imagePreview} onClick={goToCaseStudy} />
+      <h3 className="case_study_preview__title" >{title}</h3>
+      <p className="case_study_preview__subtitle" >{subtitle}</p>
+      <h4 className="case_study_preview__link" onClick={goToCaseStudy}>Read Case Study</h4>
     </div>
   );
 }
