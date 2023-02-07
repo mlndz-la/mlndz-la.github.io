e23182d4c0c60ee83f632dff0296855b7bd83efd
Janis-H <janis_h.aguilar@yahoo.com>
2023-02-07 00:00:16 -0800

    View More button created

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from 'react';
 import { caseStudiesData } from './data/CaseStudies';
 import { GetInTouch } from "./reusable/GetInTouch";
 import { MyWork } from './reusable/MyWork';
@@ -8,9 +9,22 @@ import './Home.css';
 export const Home = () => {
   const { caseStudies } = caseStudiesData;
 
+  const [displayedCaseStudies, setDisplayedCaseStudies] = useState(caseStudies.slice(0, 4));
+
+  const showViewMore = useMemo(() => !(displayedCaseStudies.length === caseStudies.length), [displayedCaseStudies]);
+
+
+  const viewMore = () => {
+    const currentViewIndex = displayedCaseStudies.length + 1
+    const newViewIndex = currentViewIndex + 4;
+
+    setDisplayedCaseStudies(caseStudies.slice(0, newViewIndex));
+  }
+
   return (
-    <div className="home_page">
-      <MyWork caseStudies={caseStudies} />
+    <div className="home_page__container">
+      <MyWork caseStudies={displayedCaseStudies} />
+      {showViewMore && <h3 className='home_page__button' onClick={viewMore}>View More</h3>}
       <GetInTouch />
     </div>
   );
