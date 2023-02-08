d8888a678c07f1f6eac4c20f877b6a451ae8b842
Janis-H <janis_h.aguilar@yahoo.com>
2023-02-08 02:57:32 -0800

    Resume page mostly completed
    Pdf image missing

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,1 +1,9 @@
-export const Resume = () => (<p>Resume</p>)
+import { ResumeIconSection } from "./reusable/ResumeIconSection";
+import './Resume.css';
+
+export const Resume = () => (
+  <div className="resume_page__container">
+    <h2 className="resume_page__title">Resume</h2>
+    <ResumeIconSection />
+  </div>
+);
