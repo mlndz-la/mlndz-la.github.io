0c7175b2a9c9f5e8feabcec3d0b70d7ffa258688
Erick Mlndz <erickmlndz@protonmail.com>
2023-07-19 17:01:15 -0700

    done

--- src/PawFinder/UsabilityStudyPF.tsx
+++ src/PawFinder/UsabilityStudyPF.tsx
@@ -8,6 +8,7 @@ export const UsabilityStudyPF = ({data, textColor}: any) => {
       <div>
         <h1 className="usability_title_PF">{usability.title}</h1>
         <p className={`${textColor}65 font_size_desc`}>{usability.desc1}</p>
+        <br/>
         <p className={`${textColor}65 font_size_desc`}>{usability.desc2}</p>
       </div>
       <div className="study_results_text_container_PF">
