c68bda542f0e6ff0bf5d3bad71cc280f1d101513
Erick Mlndz <erickmlndz@protonmail.com>
2023-07-28 13:09:14 -0700

    complete

---
+++ src/CosmicKitchen/GlobalMarketResearchCK.tsx
@@ -0,0 +1,53 @@
+import "./GlobalMarketResearchCK.css";
+
+export const GlobalMarketResearchCK = ({
+  globalMarketResearchData,
+  targetAudienceData,
+  usersData,
+  textColor,
+}: any) => {
+  const userList = usersData.userList.map((string: string, i: number) => (
+    <li className={`font_size_desc user_list_item_ck ${textColor}`} key={i}>
+      {string}
+    </li>
+  ));
+
+  return (
+    <div className="gmr_container_ck">
+      <div className="gmr_text_container_ck">
+        <h1
+          className={`font_size_main_title margin_bottom_main_title ${textColor}`}
+        >
+          {globalMarketResearchData.title}
+        </h1>
+        <p className={`font_size_desc ${textColor}50`}>
+          {globalMarketResearchData.desc}
+        </p>
+      </div>
+      <div className="gmr_image_container_ck">
+        <img className="gmr_image_ck" src={globalMarketResearchData.image} />
+      </div>
+
+      <div className="ta_text_container_ck">
+        <h1
+          className={`font_size_main_title margin_bottom_main_title ${textColor}`}
+        >
+          {targetAudienceData.title}
+        </h1>
+        <p className={`font_size_desc ${textColor}50`}>
+          {targetAudienceData.desc}
+        </p>
+      </div>
+      <div className="ta_image_container_ck">
+        <img className="ta_image_ck" src={targetAudienceData.image} />
+      </div>
+
+      <div className="users_text_container_ck">
+        <h1 className={`font_size_main_title ${textColor}50`}>
+          {usersData.title}
+        </h1>
+        <ul className="user_list_container_ck">{userList}</ul>
+      </div>
+    </div>
+  );
+};
