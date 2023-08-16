9ca9583a5b65f502f1c5a98ecb4901ae0aeca13c
Erick Mlndz <erickmlndz@protonmail.com>
2023-08-16 13:03:58 -0700

    donje

---
+++ src/TlayoasCosina/WhyThisProductTC.tsx
@@ -0,0 +1,19 @@
+import "./WhyThisProductTC.css";
+
+export const WhyThisProductTC = ({ data, textColor }: any) => {
+  const { title, desc, image } = data;
+
+  return (
+    <div className="wtp_container_tc">
+      <div className="wtp_text_container_tc">
+        <h1
+          className={`font_size_main_title margin_bottom_main_title ${textColor}`}
+        >
+          {title}
+        </h1>
+        <p className={`font_size_desc ${textColor}50`}>{desc}</p>
+      </div>
+      <img className="wtp_image_tc" src={image} alt="crocodile cartoon" />
+    </div>
+  );
+};
