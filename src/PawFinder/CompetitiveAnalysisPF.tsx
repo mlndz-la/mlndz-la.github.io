ccaf9acfa566824121d4c5ad3250f001fe0e4f98
Erick Mlndz <erickmlndz@protonmail.com>
2023-09-06 13:37:16 -0700

    competitive

--- src/PawFinder/CompetitiveAnalysisPF.tsx
+++ src/PawFinder/CompetitiveAnalysisPF.tsx
@@ -6,43 +6,64 @@ export const CompetitiveAnalysisPF = ({
   orgsData,
   badGoodData,
   elementId,
-}:any) => {
-  const displayLogos = orgsData.map((data:any, i:any) => {
+}: any) => {
+  const displayLogos = orgsData.map((data: any, i: any) => {
     return (
       <div key={i}>
         <div className="competitive_analysis_PF_logo_content">
-          <img className={`competitive_analysis_PF_logo ${data.title}`} src={data.logo} alt={`${data.title} logo`} />
+          <img
+            className={`competitive_analysis_PF_logo ${data.title}`}
+            src={data.logo}
+            alt={`${data.title} logo`}
+          />
         </div>
-        <h2 className={`competitive_analysis_PF_logo_title ${textColor}65`}>{data.title}</h2>
+        <h2 className={`competitive_analysis_PF_logo_title ${textColor}65`}>
+          {data.title}
+        </h2>
       </div>
-    )
+    );
   });
 
-  const displayGoodBad = badGoodData.map((data:any, i:any) => {
+  const displayGoodBad = badGoodData.map((data: any, i: any) => {
     return (
       <div className={`competitive_analysis_PF_good_bad_content`} key={i}>
-        <h2 className={`competitive_analysis_PF_good_bad_title ${textColor}65`}>{data.title}</h2>
-        <p className={`competitive_analysis_PF_good_bad_desc font_size_desc`}>{data.desc}</p>
+        <h2 className={`font_size_title ${textColor}65`}>{data.title}</h2>
+        <p className={`competitive_analysis_PF_good_bad_desc font_size_desc`}>
+          {data.desc}
+        </p>
       </div>
-    )
-  })
+    );
+  });
 
-  const displayScreenshot = orgsData.map((data:any, i:any) => {
+  const displayScreenshot = orgsData.map((data: any, i: any) => {
     return (
       <div key={i} className="competitive_analysis_PF_screenshot_content">
         <div className="competitive_analysis_PF_image_container">
-          <img className="competitive_analysis_PF_screenshot" src={data.screenshot1} alt={`${data.title} screenshot 1`} />
-          <img className="competitive_analysis_PF_screenshot" src={data.screenshot2} alt={`${data.title} screenshot 2`} />
+          <img
+            className="competitive_analysis_PF_screenshot"
+            src={data.screenshot1}
+            alt={`${data.title} screenshot 1`}
+          />
+          <img
+            className="competitive_analysis_PF_screenshot"
+            src={data.screenshot2}
+            alt={`${data.title} screenshot 2`}
+          />
         </div>
-        <h2 className={`competitive_analysis_PF_screenshot_title ${textColor}65`}>{data.title}</h2>
+        <h2
+          className={`competitive_analysis_PF_screenshot_title ${textColor}65`}
+        >
+          {data.title}
+        </h2>
       </div>
-    )
-  })
+    );
+  });
+
   return (
     <div className="white_content_border" id={elementId}>
-      <div className="competitive_analysis_PF_intro_container container_margin">
-        <h1 className={`competitive_analysis_PF_title ${textColor}`}>{mainData.title}</h1>
-        <p className={`competitive_analysis_PF_desc font_size_desc ${textColor}65`}>{mainData.desc}</p>
+      <div className="competitive_analysis_PF_intro_container">
+        <h1 className={`font_size_title ${textColor}`}>{mainData.title}</h1>
+        <p className={`font_size_desc ${textColor}65`}>{mainData.desc}</p>
       </div>
       {/* Good / Bad & Logo Container */}
       <article className="competitive_analysis_PF_companies_container">
@@ -51,7 +72,9 @@ export const CompetitiveAnalysisPF = ({
           {displayLogos}
         </div>
         {/* The Good & Bad */}
-        <div className="competitive_analysis_PF_good_bad_container container_margin">
+        <div
+          className={`competitive_analysis_PF_good_bad_container ${textColor}`}
+        >
           {displayGoodBad}
         </div>
       </article>
@@ -60,5 +83,5 @@ export const CompetitiveAnalysisPF = ({
         {displayScreenshot}
       </div>
     </div>
-  )
-}
+  );
+};
