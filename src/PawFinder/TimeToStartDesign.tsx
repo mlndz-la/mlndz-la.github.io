0c4e1f98913691a9832ad1e5ebc31c2ef9a0ac25
Erick Mlndz <erickmlndz@protonmail.com>
2023-09-07 12:45:27 -0700

    start design mobile

--- src/PawFinder/TimeToStartDesign.tsx
+++ src/PawFinder/TimeToStartDesign.tsx
@@ -4,9 +4,9 @@ export const TimeToStartDesign = ({title, desc, image, textColor}: any) => {
   return (
     <div className="time_to_design_container">
       <div className="time_to_design_text_container">
-        <h1 className="time_to_design_title">{title}</h1>
+        <h1 className="font_size_title dark">{title}</h1>
         <p
-          className={`time_to_design_desc font_size_desc ${textColor}65`}>
+          className={`font_size_desc ${textColor}65`}>
         {desc}</p>
       </div>
       <img className="time_to_design_image" src={image} alt="cartoon holding website" />
