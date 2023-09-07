b6a6e2dbbdde8d1a7a8a374d3cbad3c1994b314e
Erick Mlndz <erickmlndz@protonmail.com>
2023-09-07 13:39:06 -0700

    usability mobile

--- src/PawFinder/UsabilityStudyPF.tsx
+++ src/PawFinder/UsabilityStudyPF.tsx
@@ -1,20 +1,29 @@
 import "./UsabilityStudyPF.css";
 
-export const UsabilityStudyPF = ({data, textColor}: any) => {
+export const UsabilityStudyPF = ({ data, textColor }: any) => {
   const { usability, studyResults } = data;
-  
+
   return (
     <div id={usability.elementId} className="usability_container_PF">
       <div>
-        <h1 className="usability_title_PF">{usability.title}</h1>
+        <h1
+          className={`font_size_main_title margin_bottom_main_title ${textColor}`}
+        >
+          {usability.title}
+        </h1>
         <p className={`${textColor}65 font_size_desc`}>{usability.desc1}</p>
-        <br/>
+        <br />
         <p className={`${textColor}65 font_size_desc`}>{usability.desc2}</p>
       </div>
+      <br />
       <div className="study_results_text_container_PF">
-        <h1 className="usability_title_PF">{studyResults.title}</h1>
+        <h1
+          className={`font_size_main_title margin_bottom_main_title ${textColor}`}
+        >
+          {studyResults.title}
+        </h1>
         <p className={`${textColor}65 font_size_desc`}>{studyResults.desc}</p>
       </div>
     </div>
-  )
+  );
 };
