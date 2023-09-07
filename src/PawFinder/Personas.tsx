a6e391e89ded677bc9714392aecd010e6a799af3
Erick Mlndz <erickmlndz@protonmail.com>
2023-09-07 12:43:41 -0700

    personas mobile

--- src/PawFinder/Personas.tsx
+++ src/PawFinder/Personas.tsx
@@ -4,8 +4,8 @@ export const Personas = ({title, desc, personaOne, personaTwo, textColor, elemen
   return (
     <div className="personas_content_PF white_content_border test" id={elementId}>
       <div className="personas_text_container_PF">
-        <h1 className={`personas_title_PF ${textColor}`}>{title}</h1>
-        <p className={`personas_desc_PF font_size_desc ${textColor}65`}>{desc}</p>
+        <h1 className={`font_size_main_title ${textColor}`}>{title}</h1>
+        <p className={`font_size_desc ${textColor}65`}>{desc}</p>
       </div>
       <div className="personas_image_container_PF">
         <img className="personas_image_PF personas_image_one_PF" src={personaOne} alt="persona one" />
