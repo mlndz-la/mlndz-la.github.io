720c289ea9baeef9d143c7c83e7ea753f82ef69d
Erick Mlndz <erickmlndz@protonmail.com>
2023-09-07 13:08:03 -0700

    testing mobile

--- src/PawFinder/Testing.tsx
+++ src/PawFinder/Testing.tsx
@@ -1,15 +1,19 @@
 import "./Testing.css";
 
-export const Testing = ({title, desc, image, textColor}: any) => {
+export const Testing = ({ title, desc, image, textColor }: any) => {
   return (
     <div>
       <div className="testing_text_container_PF">
-        <h1 className="testing_title_PF">{title}</h1>
-        <p className={`testing_desc_PF font_size_desc ${textColor}65`}>{desc}</p>
+        <h1
+          className={`font_size_main_title margin_bottom_main_title ${textColor}`}
+        >
+          {title}
+        </h1>
+        <p className={`font_size_desc ${textColor}65`}>{desc}</p>
       </div>
       <div className="testing_image_container_PF">
-        <img src={image} className="testing_image_PF"/>
+        <img src={image} className="testing_image_PF" />
       </div>
     </div>
-  )
+  );
 };
