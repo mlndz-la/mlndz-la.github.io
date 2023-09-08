ea02fa9ade92af7db2c75c08bf2ac4b50313f9e2
Janis-H <janis_h.aguilar@yahoo.com>
2023-09-08 16:23:31 -0700

    removed unused vairables in MyWork

--- src/reusable/MyWork/types.ts
+++ src/reusable/MyWork/types.ts
@@ -1,7 +1,5 @@
 import { caseStudiesData } from "../../data/CaseStudies";
 
 export interface MyWorkProps {
-  title?: string;
-  subtitle?: string;
   caseStudies: typeof caseStudiesData.caseStudies;
 }
