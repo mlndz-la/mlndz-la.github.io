a35ab186381f911e19288a94e760d39bff49276e
Erick Mlndz <erickmlndz@protonmail.com>
2023-11-20 13:46:19 -0800

    update email

--- src/reusable/GetInTouch/GetInTouch.tsx
+++ src/reusable/GetInTouch/GetInTouch.tsx
@@ -3,12 +3,12 @@ import './GetInTouch.css'
 
 export const GetInTouch = () => {
   const openMail = () => {
-    window.location.href = "mailto:e.melendez.designer@gmail.com?subject=Job Opportunity";
+    window.location.href = "mailto:erickmelendezdesigner@gmail.com?subject=Job Opportunity";
   }
   return (
     <div className='get_in_touch__container'>
       <h2 className='get_in_touch__title'>Get In Touch</h2>
-      <button className='get_in_touch__button' onClick={openMail}>e.melendez.designer@gmail.com</button>
+      <button className='get_in_touch__button' onClick={openMail}>erickmelendezdesigner@gmail.com</button>
       <ResumeIconSection />
     </div>
   );
